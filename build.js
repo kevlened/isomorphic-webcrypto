da0ba7dc6ba8443874a311708ee30a365818e974
kevlened <boyettel@gmail.com>
2018-01-11 08:28:55 -0800

    'this' is not defined at ES6 root

---
+++ build.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+let contents = fs.readFileSync(path.join(__dirname, 'node_modules/webcrypto-shim/webcrypto-shim.js'), 'utf8')
+contents = contents.replace(
+  `self === 'undefined' ? this : self`,
+  `self === 'undefined' ? undefined : self`
+)
+contents += '\n export default {} \n'
+fs.writeFileSync(path.join(__dirname, 'webcrypto-shim.mjs'), contents)
