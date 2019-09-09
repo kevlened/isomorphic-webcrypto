ab3e417f25689d72f282c52c6ff6b6b168a1c3ac
kevlened <boyettel@gmail.com>
2019-09-08 22:33:07 -0400

    @trust/webcrypto => node-webcrypto-ossl

--- __tests__/node.js
+++ __tests__/node.js
@@ -3,8 +3,8 @@ const crypto = require('../src/index');
 require('webcrypto-test-suite')({
   crypto,
   shouldSkip(spec) {
-    if (spec.includes('ES256') && spec.includes('verify')) return true;
-    if (spec.includes('ES384') && spec.includes('verify')) return true;
-    if (spec.includes('ES512')) return true;
+    if (spec.includes('ES256') && spec.includes('exportKey')) return true;
+    if (spec.includes('ES384') && spec.includes('exportKey')) return true;
+    if (spec.includes('ES512') && spec.includes('exportKey')) return true;
   }
 });
