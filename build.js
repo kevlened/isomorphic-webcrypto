7989434a9956b37ede0572109ffe9357c7631638
kevlened <boyettel@gmail.com>
2019-09-13 09:31:33 -0400

    Fix react-native debugging

--- build.js
+++ build.js
@@ -1,12 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
+const notice = '// section modified by isomorphic-webcrypto build';
+
 let contents = fs.readFileSync(path.join(__dirname, 'node_modules/webcrypto-shim/webcrypto-shim.js'), 'utf8')
 contents = contents.replace(
   `self === 'undefined' ? this : self`,
   `self === 'undefined' ? undefined : self`
 )
-contents += '\n export default {} \n'
+contents += `\n export default {} ${notice} \n`
 fs.writeFileSync(path.join(__dirname, 'src', 'webcrypto-shim.mjs'), contents)
 
 let linerContents = fs.readFileSync(path.join(__dirname, 'node_modules/webcrypto-liner/build/webcrypto-liner.shim.js'), 'utf8')
@@ -32,9 +34,28 @@ linerContents = linerContents.replace(
   } else {
     window = self;
   }`,
-  ''
+  notice
 );
 linerContents = linerContents.replace(/self\./g, 'window.');
-linerContents = linerContents.replace('const window$1 = self;', 'const window$1 = window;')
-linerContents += '\n module.exports = liner; \n'
+linerContents = linerContents.replace(
+  `
+  const window$1 = self;
+
+  if (nativeCrypto) {
+    Object.freeze(nativeCrypto.getRandomValues);
+  }
+
+  try {
+    delete window.crypto;
+    window$1.crypto = new Crypto$1();
+    Object.freeze(window$1.crypto);
+  } catch (e) {
+    Debug.error(e);
+  }
+
+  const crypto = window$1.crypto;
+
+  exports.crypto = crypto;
+`, `exports.crypto = new Crypto$1(); ${notice}`);
+linerContents += `\n module.exports = liner; ${notice} \n`
 fs.writeFileSync(path.join(__dirname, 'src', 'webcrypto-liner.js'), linerContents);
